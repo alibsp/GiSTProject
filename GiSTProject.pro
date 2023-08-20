dc03b7f0a87e82589691658c3923ab32ccc20964
shahab.seddigh <shahabouraie@gmail.com>
2023-08-20 16:51:00 +0330

    Initial commit of new repository
    This is the new repository of project to eleminate unnecessary files in the original repository
    
    Refer to GiSTProjectOld repo for older commit history

---
+++ GiSTProject.pro
@@ -0,0 +1,47 @@
+QT += core
+
+QT -= gui
+
+CONFIG += c++17 console
+CONFIG -= app_bundle
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+        BTree/gist_btree.cpp \
+        GiST/gist.cpp \
+        GiST/gist_cursorext.cpp \
+        GiST/gist_file.cpp \
+        GiST/gist_p.cpp \
+        GiST/gist_query.cpp \
+        #GiST/gist_support.cpp \
+        GiST/gist_ustk.cpp \
+        GiST/vec_t.cpp \
+        GiST/gist_cursor.cpp \
+        GiST/gist_htab.cpp \
+        main.cpp \
+        part.cpp
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    BTree/gist_btree.h \
+    GiST/gist.h \
+    GiST/gist_compat.h \
+    GiST/gist_cursor.h \
+    GiST/gist_cursorext.h \
+    GiST/gist_defs.h \
+    GiST/gist_ext.h \
+    GiST/gist_file.h \
+    GiST/gist_htab.h \
+    GiST/gist_p.h \
+    GiST/gist_query.h \
+    GiST/gist_support.h \
+    GiST/gist_ustk.h \
+    GiST/vec_t.h \
+    part.h
---
+++ main.cpp
@@ -0,0 +1,219 @@
+#include <QCoreApplication>
+#include <QElapsedTimer>
+#include <QDebug>
+#include "GiST/gist.h"
+#include "BTree/gist_btree.h"
+#include "GiST/gist_cursor.h"
+#include "part.h"
+
+void myPrintPredFct(
+        std::ostream& s, // what to print to
+        const vec_t& pred, // pred.ptr(0) contains pointer to 8-byte aligned predicate
+        int level)
+{
+
+}
+
+void myPrintDataFct(std::ostream& s, // what to print to
+                    const vec_t& data)
+{
+
+}
+
+
+int intTest()
+{
+    gist myGist;
+    //ایجاد پایگاه داده با افزونه BTree
+    if(myGist.create("DataInt.db", &bt_int_ext)==RCOK)
+    {
+
+        //تولید اعداد تصادفی برای درج در پایگاه داده
+        srand(time(0));
+        int lb = -10000000, ub = 10000000;
+        for (int i = 0; i < 1000; i++)
+        {
+            for (int j = 0; j < 10000; j++)
+            {
+                int key = (rand() % (ub - lb + 1)) + lb;
+                int data = (rand() % (ub - lb + 1)) + lb;
+                myGist.insert((void *) &key, sizeof(int), (void *) &data, sizeof(int));
+            }
+            myGist.flush();
+        }
+    }
+    else // File maybe Exist
+    {
+        if(myGist.open("DataInt.db")!=RCOK)
+        {
+            cerr << "Can't Open File." << endl;
+            return 0;
+        }
+    }
+
+    bt_query_t q(bt_query_t::bt_betw, new int(1200), new int(1500));
+    gist_cursor_t cursor;
+
+    if(myGist.fetch_init(cursor, &q)!=RCOK)
+    {
+        cerr << "Can't initialize cursor." << endl;
+        return(eERROR);
+    }
+
+    bool eof = false;
+    int key, data;
+    smsize_t keysz=sizeof(int), datasz=sizeof(int);
+    while (!eof)
+    {
+        if(myGist.fetch(cursor, (void *)&key, keysz, (void *)&data, datasz, eof)!=RCOK)
+        {
+            cerr << "Can't fetch from cursor." << endl;
+            return(eERROR);
+        }
+        if (!eof)
+        {
+            std::cout<<key<<"->"<<data<<endl;
+        }
+        // process key and data...
+    }
+    return 1;
+}
+
+static char *rand_string(char *str, size_t size)
+{
+    const char charset[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.-";
+
+    if (size)
+    {
+        --size;
+        for (size_t n = 0; n < size; n++)
+        {
+            int key = rand() % (int) (sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+    return str;
+}
+
+int stringTest()
+{
+    gist myGist;
+    //ایجاد پایگاه داده با افزونه BTree
+    if(myGist.create("DataString.db", &bt_str_ext)==RCOK)
+    {
+
+        //تولید رشته های تصادفی برای درج در پایگاه داده
+        srand(time(0));
+        int lb = 3, ub = 100;
+
+        for (int i = 0, k=1; i < 10000; i++)
+        {
+            for (int j = 0; j < 1000; j++)
+            {
+                char key[100]={0};
+                int len=(rand() % (ub - lb + 1)) + lb;
+                rand_string(key, len);
+                int data = k++;
+                myGist.insert((void *) &key, 100, (void *) &data, sizeof(int));
+            }
+            //12532000 + 131000 + 118000 + 166000 + 1000 + 100000
+            myGist.flush();
+            cout << "Insert "<<k-1<< " Record ok" << endl;
+        }
+    }
+    else // File maybe Exist
+    {
+        if(myGist.open("DataString.db")!=RCOK)
+        {
+            cerr << "Can't Open File." << endl;
+            return 0;
+        }
+    }
+    //char my_key[100]="Ali Aldaghi";
+//    int my_data = 1000;
+//    myGist.insert((void *) &my_key, 100, (void *) &my_data, sizeof(int));
+
+    //bt_query_t qRemove(bt_query_t::bt_eq, my_key, my_key);
+
+    //myGist.remove(&qRemove);
+
+    char key1[100] = "Ali";
+    char key2[100] = "Alizzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+
+    QElapsedTimer timer;
+    timer.start();
+
+    bt_query_t q(bt_query_t::bt_betw, key1, key2);
+    //bt_query_t q(bt_query_t::bt_eq, &key1, key2);
+    gist_cursor_t cursor;
+    if(myGist.fetch_init(cursor, &q)!=RCOK)
+    {
+        cerr << "Can't initialize cursor." << endl;
+        return(eERROR);
+    }
+    bool eof = false;
+
+    int data;
+    smsize_t keysz=100, datasz=sizeof(int);
+    char key[100]={0};
+    while (!eof)
+    {
+        if(myGist.fetch(cursor, (void *)&key, keysz, (void *)&data, datasz, eof)!=RCOK)
+        {
+            cerr << "Can't fetch from cursor." << endl;
+            return(eERROR);
+        }
+        if (!eof)
+            cout<<(char*)&key<<"->"<<data<<endl;
+        // process key and data...
+    }
+    cout<<endl<<"Time is:"<<timer.elapsed()<<endl;
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    //intTest();
+    //stringTest();
+    qDebug()<<"start...";
+
+    QString csvFile;
+#ifdef __linux__
+    //csvFile = "/media/ali/Data/Programming/Projects/Part/Data/data2.csv";
+    csvFile = "/home/mahmoudmahmoudinik/Data/data2.csv";
+#elif _WIN32
+    csvFile = "D:\\Programming\\Projects\\Part\\Data\\data.csv";
+#endif
+
+    bool clean=true;
+    Part part;
+    if(clean)
+    {
+        part.dropGists();
+        part.importCSV(csvFile);
+    }
+    else
+        part.loadGists();
+
+    QElapsedTimer timer;
+    timer.start();
+
+    QList<UUID> results=part.findKey("source_gitlab");
+    //results.append(part.findKey("updatedAt_14010510200905000"));
+    //results.append(part.findKey("createdAt_14010511150209000"));
+
+    /*uint64_t time1=timer.nsecsElapsed();
+    QSet<QString> ids(results.begin(), results.end());
+    uint64_t time=timer.nsecsElapsed();
+    for (QString res:ids)   //shahab
+    {
+        /*char* output = (char*)malloc( 37*sizeof(char) );
+        unsigned char* bins = (unsigned char*)malloc( 37*sizeof(unsigned char) );
+        memcpy( bins, res.toStdString().c_str(), 16);
+        part.binToHexStr(bins, &output);
+        qDebug() << "findKey result: -->\t" << output;
+    }*/
+    //qDebug()<<"Execute Time: "<<time1<<time<<" ns, record count:"<<ids.count();
+    qDebug()<<"finish.";
+}
