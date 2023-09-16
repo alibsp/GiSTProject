4c9bd491549575cb06507d7b8c6802f268f5980a
th3r0b0t <shahabodin.sedigh@gmail.com>
2023-09-17 00:07:04 +0330

    Fixed CMakeLists.txt syntax, added dependency-builder.sh
    In CMakeLists.txt the variable must be between dollor sign and two curly braces to work which is fixed for QT_SEARCH_PATH
    
    A shell script dependency-builder.sh has been added under thirdParty directory to automatically clone and build both abseil and re2 libraries

---
+++ thirdParty/dependency-builder.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+printf "Current working dir should be inside thirdParty, confirm it: \033[0;31m$(pwd)\n\033[0mIf it's not, use ctrl+c to stop script:\n"
+read -p "Enter to continue or ctrl+c to stop:    "
+
+[ ! -e absl/ ] || rm -rf absl/
+[ ! -e re2/ ] || rm -rf re2
+
+mkdir sources
+cd sources
+git clone https://github.com/abseil/abseil-cpp.git
+git clone https://github.com/google/re2.git
+
+clear
+
+cd abseil-cpp
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../../absl
+cmake --build . --target install
+
+
+clear
+
+cd ../../
+cd re2
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../../../re2 -Dabsl_DIR=../../../absl/lib/cmake/absl/
+cmake --build . --target install
+
+clear
+
+cd ../../../
+rm -rf sources
